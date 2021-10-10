9277dcdd8f72a99e6e38c50c933cfccdb46ede14
zdenop <zdenop@gmail.com>
2021-10-10 08:43:57 +0200

    add link.php (in slovak)

---
+++ www/links.php
@@ -0,0 +1,28 @@
+<table cellspacing="2" cellpadding="0" border="0">
+<tr>
+<td>
+	<strong>Odkazy</strong>
+	<ul style="margin-top: 0px;margin-bottom: 0px">
+		<li><a href="background.php">Pozadie projektu (nemecky)</a></li>
+		<li><a href="http://lists.berlios.de/mailman/listinfo/openthesaurus-discuss">E-mailová konferencia OpenThesaurus</a></li>
+		<li><a href="http://sk.wikipedia.org">Slovenská Wikipedia</a> / <a href="http://sk.wiktionary.org">Wikislovník</a></li>
+		<li>Anglické synonymické slovníky:
+			<ul>
+			<li><a href="http://thesaurus.reference.com/">Thesaurus.com</a></li>
+			<li><a href="http://www.cogsci.princeton.edu/~wn/">Wordnet</a></li>
+			</ul>
+		</li>
+		<li>OpenThesaurus:
+            <a href="http://www.opentaal.org/opentaalbank/thesaurus">holandský</a>,
+	   		 <!-- <a href="http://synonymer.merg.net/">nórsky</a>,  -->
+			<a href="http://www.openthesaurus.de/">nemecký</a>,
+			<a href="http://synonimy.sourceforge.net/">polský</a>,
+			<a href="http://openthesaurus.caixamagica.pt/">portugalský</a>,
+			<!-- <a href="http://www.openthesaurus.tk">slovenský</a>, -->
+			<a href="http://www.tezaver.si/">slovinský</a>,
+			<a href="http://openthes-es.berlios.de">španielsky</a>
+		</li>
+	</ul>
+</td>
+</tr>
+</table>

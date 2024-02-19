0ddfa2adc43249ca967250cb3c2d9c686cd26191
zdenop <zdenop@gmail.com>
2024-02-19 20:16:58 +0100

    fix paths for (sk) xgettext

--- www/include/locale/sk/LC_MESSAGES/make.sh
+++ www/include/locale/sk/LC_MESSAGES/make.sh
@@ -2,6 +2,6 @@
 #dnaber, 2003-05-05
 
 mv messages.po old.po
-xgettext -n ../../../../include/phplib/loginform.html ../../../../include/*.php ../../../../www/*.php -o messages.po --keyword=_
+xgettext -n ../../../../include/phplib/*.php ../../../../include/*.php ../../../../*.php -o messages.po --keyword=_
 msgmerge old.po messages.po --output-file=messages.po
 echo "Update translations (e.g. using KBabel), then call 'msgfmt messages.po'."
